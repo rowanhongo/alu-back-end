3166332c4b416ef0355caf3634ba2a948b4d6416
Rowan Hongo <r.hongo@alustudent.com>
2025-07-03 22:38:21 +0200

    br

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
 """ Import libraries """
 
+import requests
 import sys
-import urllib.request
-import json
 
 """Gathering data from an API """
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
     employee_id = sys.argv[1]
-
     url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
 
-    with urllib.request.urlopen(url) as response:
-        user_info = json.loads(response.read().decode())
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
 
-    with urllib.request.urlopen(todo_url) as response:
-        todo_info = json.loads(response.read().decode())
+    user_info = requests.request("GET", url).json()
+    todo_info = requests.request("GET", todo).json()
 
     employee_name = user_info.get("name")
-    completed_tasks = list(filter(lambda x: x.get("completed") is True, todo_info))
-    number_done = len(completed_tasks)
-    total_tasks = len(todo_info)
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_com = len(total_tasks)
+    total_task_done = len(todo_info)
 
     print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          number_done, total_tasks))
+          task_com, total_task_done))
 
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+    [print("\t {}".format(task.get("title"))) for task in total_tasks]
